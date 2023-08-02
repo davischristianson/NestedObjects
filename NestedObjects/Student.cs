880ef5d1307949755aec5e13ca5eece4eae1e1b0
davischristianson <dav.christian10@gmail.com>
2023-08-02 15:51:40 -0700

    Added missing documentation and date of birth

--- NestedObjects/Student.cs
+++ NestedObjects/Student.cs
@@ -26,8 +26,19 @@ namespace NestedObjects
         /// </summary>
         public string SchoolEmail { get; set; }
 
+        /// <summary>
+        /// The student's date of birth
+        /// </summary>
+        public DateOnly DateOfBirth { get; set; }
+
+        /// <summary>
+        /// Students preferred contact number
+        /// </summary>
         public string PhoneNumber { get; set; }
 
+        /// <summary>
+        /// The advisor assigned to the student
+        /// </summary>
         public Advisor AssignedAdvisor { get; set; }
     }
 }
